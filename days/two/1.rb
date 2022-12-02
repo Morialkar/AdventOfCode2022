60615097c3b270703710ef065f1d67b219c3e51d
Philippe Gilbert <philippe@morialkar.com>
2022-12-02 12:55:17 -0500

    :sparkles: Day Two

--- days/two/1.rb
+++ days/two/1.rb
@@ -1,1 +1,33 @@
-A = "Hello, world!"
+STRATEGY = Array.new
+
+CHOICE_VALUE = Hash["X" => 1, "Y" => 2, "Z" => 3]
+OUTCOME_VALUE = Hash["LOST" => 0, "DRAW" => 3, "WIN" => 6]
+
+part_one_answer = 0
+
+STATES =
+  Hash[
+    "DRAW" => Array["A X", "B Y", "C Z"],
+    "WIN" => Array["A Y", "B Z", "C X"],
+    "LOST" => Array["A Z", "B X", "C Y"]
+  ]
+
+File
+  .readlines("./inputs.txt")
+  .each do |line|
+    key = String.new(line).split("/")[0].split(" ")[0]
+    value = String.new(line).split("/")[0].split(" ")[1]
+    STRATEGY.push(Array[key, value])
+  end
+
+STRATEGY.each do |fight|
+  state = "LOST"
+  STATES.each do |name, state_possibility|
+    state = name if state_possibility.include? fight.join(" ")
+  end
+  part_one_answer += CHOICE_VALUE[fight[1]] + OUTCOME_VALUE[state]
+end
+
+PART_ONE_VALUE = part_one_answer
+
+puts "Answer to Part One: #{PART_ONE_VALUE}"
--- days/two/2.rb
+++ days/two/2.rb
@@ -2,7 +2,26 @@ begin
   load File.expand_path("../1.rb", __FILE__)
 rescue LoadError
 end
-A ||= "."
 
-puts A
-exit(0)
+part_two_answer = 0
+
+STRATEGY_STATES = Hash["X" => "LOST", "Y" => "DRAW", "Z" => "WIN"]
+
+fight_against = ""
+choice_val = 0
+
+STRATEGY.each do |fight|
+  outcome_val = OUTCOME_VALUE[STRATEGY_STATES[fight[1]]]
+  fight_against = fight[0]
+  STATES[STRATEGY_STATES[fight[1]]].each do |possible_fight|
+    if possible_fight.include? fight_against
+      choice_val = CHOICE_VALUE[possible_fight.split(" ")[1]]
+    end
+  end
+
+  part_two_answer += choice_val + outcome_val
+end
+
+PART_TWO_VALUE = part_two_answer
+
+puts "Answer to Part Two: #{PART_TWO_VALUE}"
